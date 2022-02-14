e3dd076ddaa36fbcf49b497fe117c8473a4cb39e
Nathan Harper <nathan.harper@aenetworks.com>
2022-02-14 13:02:35 -0500

    storybook setup

---
+++ src/stories/TestComponent.stories.jsx
@@ -0,0 +1,12 @@
+import TestComponent from '../TestComponent';
+
+export default {
+  title: 'TestComponent',
+  component: TestComponent,
+};
+
+const Template = (args) => <TestComponent {...args} />;
+
+export const Default = {
+  args: { },
+};
